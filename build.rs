11ea954dd40318eed6c8a70eee38e4bd3f6342bc
Andreas Franzén <andreas@devil.se>
2019-02-15 11:09:20 +0100

    ..

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::probe_library("vips").unwrap();
+    std::env::set_var("PKG_CONFIG_PATH", "/var/task/lib/pkgconfig");
+    pkg_config::Config::new().statik(false).probe("vips").unwrap();
+    //pkg_config::probe_library("vips").unwrap();
 }
